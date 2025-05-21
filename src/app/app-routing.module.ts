a73f33bc83fd060e89af3d8d490c7cf95cfa0181
Nicolas <Nicolasgabrielsanchez7@gmail.com>
2025-05-21 18:08:50 -0300

    añado vista de socio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,35 +4,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'landing',
-    pathMatch: 'full'
+    loadChildren: () => import('./socio/tabs/tabs.module').then(m => m.TabsPageModule)
+    
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => import('./socio/home/home.module').then( m => m.HomePageModule) // es la pagina de inicio ' ' 
+    
   },
+  
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    path: 'tabs',
+    loadChildren: () => import('./socio/tabs/tabs.module').then( m => m.TabsPageModule)
   },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  },
-  {
-    path: 'reset-password',
-    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
-  },
-  {
-    path: 'landing',
-    loadChildren: () => import('./landing/landing.module').then( m => m.LandingPageModule)
-  },
+  
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
